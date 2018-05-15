6ed2a26146a5184649b3099421728bd9f24b38ad
Patrick Olvera <patrickolvera@gmail.com>
2018-05-14 21:13:03 -0500

    feat: shown cards are put in a list

--- js/app.js
+++ js/app.js
@@ -43,17 +43,34 @@ function createCardsEl() {
 createCardsEl();
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * 
+ *  
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-deck.addEventListener('click', showCard);
 
-function showCard (event) {
- event.target.classList += " show open";
+/* 
+* set up the event listener for a card. If a card is clicked:
+*/
+
+deck.addEventListener('click', onCardClick);
+
+const cardsOpen = [];
+const cardsMatched = [];
+
+function onCardClick(event) {
+    showCard(event);
+    addToOpen(event);
+}
+// - display the card's symbol (put this functionality in another function that you call from this one)
+function showCard(event) {
+    event.target.classList += " show open";
+}
+
+//- add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+function addToOpen(event) {
+    cardsOpen.push(event.target.firstChild.classList[1]);
 }
